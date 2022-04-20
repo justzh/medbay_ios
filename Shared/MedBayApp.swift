29b3e1ded997bbbaa16661af746e4a0a61eb7e25
Justin Zhang <31813850+justzh@users.noreply.github.com>
2022-04-20 11:26:13 -0400

    Initial Commit

---
+++ Shared/MedBayApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedBayApp.swift
+//  Shared
+//
+//  Created by Justin Zhang on 4/20/22.
+//
+
+import SwiftUI
+
+@main
+struct MedBayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
